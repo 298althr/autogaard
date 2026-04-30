2e8a8f375d5e178d7bda3bd94e13bcf42daa4f8f
298althr <298althr@users.noreply.github.com>
2026-04-30 10:28:15 -0700

    Cleanup: Purged legacy assets and research files to align with production standards.

--- scratch/extract_xml_text.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('c:\\Users\\Sav-Dev\\Documents\\Autogaard\\Autogaard\\docx_temp\\word\\document.xml', 'utf8');
-const regex = /<w:t[^>]*>([^<]+)<\/w:t>/g;
-let match;
-while ((match = regex.exec(content)) !== null) {
-  console.log(match[1]);
-}
